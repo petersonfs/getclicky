1abbb2cb12f5575cbdfa51c802d2f9eff1b2e490
Bobby Uhlenbrock <bobby@helloample.com>
2013-07-25 11:10:00 -0400

    Allow response object to be initialized with a string as well as an array

--- spec/getclicky/response_spec.rb
+++ spec/getclicky/response_spec.rb
@@ -1,36 +1,67 @@
 require "spec_helper"
 
 describe Getclicky::Response do
-  subject {
-    Getclicky::Response.new([{
-      "type"=>"pages",
-      "dates"=>
-        [{"date"=>"2011-09-27",
-          "items"=>
-          [{"value"=>"6",
-            "value_percent"=>"66.7",
-            "title"=>"Test Page",
-            "stats_url"=>"http://getclicky.com/stats/visitors",
-            "url"=>"http://blackbookemg.dev/posts"},
-          {"value"=>"1",
-            "value_percent"=>"11.1",
-            "title"=>"Test Page 2",
-            "stats_url"=>
-            "http://getclicky.com/stats/visitors",
-            "url"=>"http://blackbookemg.dev/requests/1"}
-          ]}
-        ]
-      }])
-  }
   
-  its(:item) { should be_an_instance_of Array }
-  its(:data) { should be_an_instance_of Array }
+  context "when initialized with an Array" do
+    
+    subject {
+      Getclicky::Response.new([{
+        "type"=>"pages",
+        "dates"=>
+          [{"date"=>"2011-09-27",
+            "items"=>
+            [{"value"=>"6",
+              "value_percent"=>"66.7",
+              "title"=>"Test Page",
+              "stats_url"=>"http://getclicky.com/stats/visitors",
+              "url"=>"http://blackbookemg.dev/posts"},
+            {"value"=>"1",
+              "value_percent"=>"11.1",
+              "title"=>"Test Page 2",
+              "stats_url"=>
+              "http://getclicky.com/stats/visitors",
+              "url"=>"http://blackbookemg.dev/requests/1"}
+            ]}
+          ]
+        }])
+    }
   
-  context "when using" do
+    its(:item) { should be_an_instance_of Array }
+    its(:data) { should be_an_instance_of Array }
+  
+  
     it "should return an array of Hashie objects" do
       subject.data.each do |d|
         d.should be_an_instance_of Hashie::Mash
       end
     end
+    
   end
+
+  context "when initialized with a String" do
+    
+    subject {
+      Getclicky::Response.new("[{\"type\":\"pages\",
+        \"dates\":[
+          {\"date\":\"2011-09-27\",
+            \"items\":[
+              {\"value\":\"6\",\"value_percent\":\"66.7\",\"title\":\"Test Page\",\"stats_url\":\"http://getclicky.com/stats/visitors\",\"url\":\"http://blackbookemg.dev/posts\"},
+              {\"value\":\"1\",\"value_percent\":\"11.1\",\"title\":\"Test Page 2\",\"stats_url\":\"http://getclicky.com/stats/visitors\",\"url\":\"http://blackbookemg.dev/requests/1\"}
+            ]}
+          ]}
+        ]")
+    }
+  
+    its(:item) { should be_an_instance_of String }
+    its(:data) { should be_an_instance_of Array }
+  
+  
+    it "should return an array of Hashie objects" do
+      subject.data.each do |d|
+        d.should be_an_instance_of Hashie::Mash
+      end
+    end
+    
+  end
+  
 end
